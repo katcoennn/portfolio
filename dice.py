e68d774f1d620d108b87e4b72131a15fa00dd029
Kathleen Coen <kcoen29@gmail.com>
2017-07-21 09:50:47 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,6 +1,6 @@
 from random import randint
 dice = randint (1,6)
-guess = str(input("Guess a number between 1 and 6!"))
+guess = str(input("Guess a number between 1 and 6: "))
 answer = str(dice)
 if guess == answer:
     print ("You are correct! The dice rolled the number "+answer+"!")
