eda79f077e9bed2733bc83791475d4e3b6a63bb8
Kathleen Coen <kcoen29@gmail.com>
2017-07-21 09:47:45 -0400

    Delete dice.py

--- dice.py
+++
@@ -1,9 +0,0 @@
-import random
-dice = [1,2,3,4,5,6]
-a = (random.choice(dice))
-print ("What number will the dice roll?")
-user_input = input()
-if user_input == a:
-    print ("You are correct! The dice rolled the number "+a+"!")
-else:
-    print ("You are incorrect! The dice rolled the number "+a+"!")
